4c8ed910eae311bfd7aa5179c6c3eda46a2271a9
elboqueronpaco <elboqueronpaco@gmail.com>
2021-02-01 10:09:58 +0100

    elminar nodemodule

---
+++ apps/api/nest-api/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_USERNAME } from './config/constants';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type:'mysql',
+        host: config.get<string>(DATABASE_HOST),
+        port: parseInt(config.get<string>(DATABASE_PORT), 10),
+        username: config.get<string>(DATABASE_USERNAME),
+        password: config.get<string>(DATABASE_PASSWORD),
+        database: config.get<string>(DATABASE_NAME),
+        entities: [__dirname + './**/**/*entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+        logger: 'file'
+      })
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }), 
+    PostModule,
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
